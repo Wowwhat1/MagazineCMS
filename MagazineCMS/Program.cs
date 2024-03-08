26dbc8fc1048efae0c8ab45130629ebaca587752
Wowwhat1 <86320782+Wowwhat1@users.noreply.github.com>
2024-03-08 15:03:52 +0700

    PP-40/fix: fix login/register 404 error

--- MagazineCMS/Program.cs
+++ MagazineCMS/Program.cs
@@ -11,6 +11,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
@@ -27,7 +28,9 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
+app.MapRazorPages(); 
 
 app.MapControllerRoute(
     name: "default",
