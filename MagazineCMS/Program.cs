b79d3b8629486baaa92c15b79f5d75eda6faa652
Minh8x8 <triminh701@gmail.com>
2024-03-09 11:09:41 +0700

    PP-21/feat: Register account

--- MagazineCMS/Program.cs
+++ MagazineCMS/Program.cs
@@ -17,7 +17,8 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddIdentity<IdentityUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = false).
-    AddEntityFrameworkStores<ApplicationDbContext>();
+    AddEntityFrameworkStores<ApplicationDbContext>().
+    AddDefaultTokenProviders();
 builder.Services.AddRazorPages();
 builder.Services.AddScoped<IEmailSender, EmailSender>();
 // Add scoped
