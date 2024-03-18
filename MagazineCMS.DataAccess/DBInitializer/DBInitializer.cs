0c1bada831be5403080eb18a230e90918720ba27
Minh8x8 <triminh701@gmail.com>
2024-03-19 00:06:33 +0700

    No ticket: Seed DB

--- MagazineCMS.DataAccess/DBInitializer/DBInitializer.cs
+++ MagazineCMS.DataAccess/DBInitializer/DBInitializer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -37,7 +38,10 @@ namespace MagazineCMS.DataAccess.DBInitializer
                     _db.Database.Migrate();
                 }
             }
-            catch (Exception ex) { }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error applying migrations: {ex.Message}");
+            }
 
             //create roles if they are not created
             if (!_roleManager.RoleExistsAsync(SD.Role_Student).GetAwaiter().GetResult())
@@ -47,23 +51,69 @@ namespace MagazineCMS.DataAccess.DBInitializer
                 _roleManager.CreateAsync(new IdentityRole(SD.Role_Admin)).GetAwaiter().GetResult();
                 _roleManager.CreateAsync(new IdentityRole(SD.Role_Manager)).GetAwaiter().GetResult();
 
-                //if roles are not created, then we will create admin user as well
                 _userManager.CreateAsync(new User
                 {
+                    Id = "AdminID1",
                     UserName = "admin@gmail.com",
                     Email = "admin@gmail.com",
-                    Firstname = "Admin",
-                    Lastname = "PublishPro",
+                    Firstname = "A",
+                    Lastname = "Nguyen Van",
                     PhoneNumber = "1112223333",
-                    AvatarUrl = "test 123 Ave",
+                    AvatarUrl = SD.Default_Avatar,
                     FacultyId = 1,
                 }, "Admin123*").GetAwaiter().GetResult();
 
-                User user = _db.Users.FirstOrDefault(u => u.Email == "admin@gmail.com");
-                _userManager.AddToRoleAsync(user, SD.Role_Admin).GetAwaiter().GetResult();
+                    User userDB = _db.Users.FirstOrDefault(u => u.Email == "admin@gmail.com");
+                    _userManager.AddToRoleAsync(userDB, SD.Role_Admin).GetAwaiter().GetResult();
+
+                _userManager.CreateAsync(new User
+                {
+                    Id = "ManagerID1",
+                    UserName = "manager@gmail.com",
+                    Email = "manager@gmail.com",
+                    Firstname = "B",
+                    Lastname = "Nguyen Thi",
+                    PhoneNumber = "1112223333",
+                    AvatarUrl = SD.Default_Avatar,
+                    FacultyId = 1,
+                }, "Admin123*").GetAwaiter().GetResult();
+
+                 userDB = _db.Users.FirstOrDefault(u => u.Email == "manager@gmail.com");
+                _userManager.AddToRoleAsync(userDB, SD.Role_Manager).GetAwaiter().GetResult();
+
+                _userManager.CreateAsync(new User
+                {
+                    Id = "CoordinatorID1",
+                    UserName = "coordinator@gmail.com",
+                    Email = "coordinator@gmail.com",
+                    Firstname = "C",
+                    Lastname = "Dang Van",
+                    PhoneNumber = "1112223333",
+                    AvatarUrl = SD.Default_Avatar,
+                    FacultyId = 2,
+                }, "Admin123*").GetAwaiter().GetResult();
+
+                userDB = _db.Users.FirstOrDefault(u => u.Email == "coordinator@gmail.com");
+                _userManager.AddToRoleAsync(userDB, SD.Role_Coordinator).GetAwaiter().GetResult();
+
+                _userManager.CreateAsync(new User
+                {
+                    Id = "StudentID1",
+                    UserName = "student@gmail.com",
+                    Email = "student@gmail.com",
+                    Firstname = "D",
+                    Lastname = "Dang Thi",
+                    PhoneNumber = "1112223333",
+                    AvatarUrl = SD.Default_Avatar,
+                    FacultyId = 2,
+                }, "Admin123*").GetAwaiter().GetResult();
+
+                userDB = _db.Users.FirstOrDefault(u => u.Email == "student@gmail.com");
+                _userManager.AddToRoleAsync(userDB, SD.Role_Student).GetAwaiter().GetResult();
+
             }
 
-            return;
         }
+
     }
 }
---
+++ MagazineCMS.DataAccess/Migrations/20240318161333_AddSeedDataMagazineContributionDocument.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace MagazineCMS.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSeedDataMagazineContributionDocument : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Contributions_AspNetUsers_UserId",
+                table: "Contributions");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Contributions_Magazines_MagazineId",
+                table: "Contributions");
+
+            migrationBuilder.InsertData(
+                table: "Magazines",
+                columns: new[] { "Id", "Description", "EndDate", "FacultyId", "Name", "SemesterId", "StartDate" },
+                values: new object[,]
+                {
+                    { 1, "Welcome to the Spring 2024 issue of Cutting-Edge Tech, your ultimate guide to the latest innovations and developments in the world of computing. In this edition, we delve into the forefront of technology, exploring groundbreaking advancements that are shaping the future of computing.", new DateTime(2024, 3, 25, 23, 13, 32, 243, DateTimeKind.Local).AddTicks(6054), 2, "Computing Magazine - Spring 2024", 1, new DateTime(2024, 3, 11, 23, 13, 32, 243, DateTimeKind.Local).AddTicks(6031) },
+                    { 2, "Welcome", new DateTime(2024, 3, 25, 23, 13, 32, 243, DateTimeKind.Local).AddTicks(6060), 3, "Business Magazine - Spring 2024", 1, new DateTime(2024, 3, 11, 23, 13, 32, 243, DateTimeKind.Local).AddTicks(6059) }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Contributions",
+                columns: new[] { "Id", "MagazineId", "Status", "SubmissionDate", "Title", "UserId" },
+                values: new object[] { 1, 1, "Pending", new DateTime(2024, 3, 18, 23, 13, 32, 243, DateTimeKind.Local).AddTicks(6091), "The Future of AI", "StudentID1" });
+
+            migrationBuilder.InsertData(
+                table: "Documents",
+                columns: new[] { "Id", "ContributionId", "DocumentUrl", "Type" },
+                values: new object[] { 1, 1, "~/contributions/StudentID1/File.docx", "Word" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Contributions_AspNetUsers_UserId",
+                table: "Contributions",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Contributions_Magazines_MagazineId",
+                table: "Contributions",
+                column: "MagazineId",
+                principalTable: "Magazines",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Contributions_AspNetUsers_UserId",
+                table: "Contributions");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Contributions_Magazines_MagazineId",
+                table: "Contributions");
+
+            migrationBuilder.DeleteData(
+                table: "Documents",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Magazines",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Contributions",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Magazines",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Contributions_AspNetUsers_UserId",
+                table: "Contributions",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Contributions_Magazines_MagazineId",
+                table: "Contributions",
+                column: "MagazineId",
+                principalTable: "Magazines",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
