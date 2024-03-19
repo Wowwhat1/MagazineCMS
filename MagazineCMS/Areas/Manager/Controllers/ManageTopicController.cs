c3fb926dd642dbd5ac91805bc450f5d30f50bcfd
TBot <TrongThinh2602@gmail.com>
2024-03-20 01:21:17 +0700

    PP 19 And 36 - Manager - Show table magazine

---
+++ MagazineCMS/Areas/Manager/Controllers/ManageTopicController.cs
@@ -0,0 +1,41 @@
+﻿using MagazineCMS.DataAccess.Repository;
+using MagazineCMS.DataAccess.Repository.IRepository;
+using MagazineCMS.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagazineCMS.Areas.Manager.Controllers
+{
+    [Area("Manager")]
+    public class ManageTopicController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ManageTopicController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            string userEmail = User.Identity.Name;
+            int userFaculty = _unitOfWork.User.Get(x => x.Email == userEmail).FacultyId;
+
+            List<Magazine> magazineList = _unitOfWork.Magazine.GetAll(includeProperties: "Faculty,Semester").ToList();
+            List<Magazine> closedMagazines = magazineList.Where(m => m.EndDate <= DateTime.Now).ToList();
+            List<Magazine> openMagazines = magazineList.Where(m => m.EndDate > DateTime.Now).ToList();
+
+            return Json(new { data = magazineList, closedMagazines, openMagazines });
+        }
+
+        #endregion
+    }
+}
