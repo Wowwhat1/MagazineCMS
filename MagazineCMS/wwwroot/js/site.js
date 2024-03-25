3ac2946f2d88612472ef7ac5e61d8082c8b718e2
TBot <TrongThinh2602@gmail.com>
2024-03-25 22:10:19 +0700

    PP 64- Manager - Fix active option sidebar

--- MagazineCMS/wwwroot/js/site.js
+++ MagazineCMS/wwwroot/js/site.js
@@ -2,3 +2,26 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+//Active item option selected sidebar
+$(document).ready(function () {
+    var currentUrl = window.location.href;
+    $('.nav-item a').each(function () {
+        var linkUrl = this.href;
+        if (currentUrl == linkUrl) {
+            $(this).closest('.nav-item').addClass('active');
+        }
+    });
+});
+
+//Add toggled into class sidebar when click
+//$(document).ready(function () {
+//    var currentUrl = window.location.href;
+//    $('.navbar-nav .nav-item a').each(function () {
+//        var linkUrl = this.href;
+//        if (currentUrl == linkUrl) {
+//            $(this).closest('.nav-item').addClass('toggled');
+//        }
+//    });
+//});
