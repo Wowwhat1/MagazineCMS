5d31d6d2baf9495a61d7fde6d893094ed83bf0e1
Minh8x8 <triminh701@gmail.com>
2024-03-28 21:45:30 +0700

    PP-79: In app notification repository

---
+++ MagazineCMS.DataAccess/Repository/NotificationRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using MagazineCMS.DataAccess.Data;
+using MagazineCMS.DataAccess.Repository.IRepository;
+using MagazineCMS.Models;
+
+namespace MagazineCMS.DataAccess.Repository
+{
+    public class NotificationRepository : Repository<Notification>, INotificationRepository
+    { 
+        private ApplicationDbContext _db;
+
+        public NotificationRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Notification obj)
+        {
+            _db.Notifications.Update(obj);
+        }
+    }
+}
