d243409a41800f4235319435854e32d66f17a350
Minh8x8 <triminh701@gmail.com>
2024-04-01 00:59:01 +0700

    PP-79: Dashboard for coordinator

--- MagazineCMS/Areas/Coordinator/Controllers/HomeController.cs
+++ MagazineCMS/Areas/Coordinator/Controllers/HomeController.cs
@@ -1,12 +1,46 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MagazineCMS.DataAccess.Repository.IRepository;
+using MagazineCMS.Models;
+using MagazineCMS.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MagazineCMS.Areas.Coordinator.Controllers
 {
+    [Area("Coordinator")]
+    [Authorize(Roles = SD.Role_Coordinator)]
     public class HomeController : Controller
     {
+        private readonly IUnitOfWork _unitOfWork;
+        public HomeController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
         public IActionResult Index()
         {
-            return View();
+            var countContributionApproved = 0;
+            var countContributionPending = 0;
+            var countContributionRejected = 0;
+
+            DateTime currentDate = DateTime.Now;
+            var currentSemester = _unitOfWork.Semester.Get(s => s.StartDate <= currentDate && currentDate <= s.EndDate);
+            if (currentSemester == null)
+            {
+                //get all close semester
+                var semesters = _unitOfWork.Semester.GetAll(s => s.EndDate <= currentDate ).ToList();
+                currentSemester = semesters[semesters.Count -1];
+            }
+            // get contributions
+            var magazineIds = _unitOfWork.Magazine.GetAll(m => m.SemesterId == currentSemester.Id).Select(m => m.Id).ToList();
+            foreach (var magazineId in magazineIds)
+            {
+                var a = _unitOfWork.Contribution.GetAll(c => c.MagazineId == magazineId && c.Status == SD.Status_Approved).ToList().Count;
+                var b = _unitOfWork.Contribution.GetAll(c => c.MagazineId == magazineId && c.Status == SD.Status_Pending).ToList().Count;
+                var c = _unitOfWork.Contribution.GetAll(c => c.MagazineId == magazineId && c.Status == SD.Status_Rejected).ToList().Count;
+                countContributionApproved += a;
+                countContributionPending += b;
+                countContributionPending += c;       
+            }
+            return View(new Tuple<Semester, int, int, int>(currentSemester, countContributionApproved, countContributionPending, countContributionRejected));
         }
     }
 }
