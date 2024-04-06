a552d1afae5888bbdc6cabc25df98013a901131f
nguyendoanminhhieu <hieundmgcd210003@fpt.edu.vn>
2024-04-06 22:41:08 +0700

    PP-112--List contribution route to magazine controller

--- MagazineCMS/Areas/Student/Controllers/HomeController.cs
+++ MagazineCMS/Areas/Student/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
+using MagazineCMS.DataAccess.Repository.IRepository;
 using MagazineCMS.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Security.Claims;
 
 namespace MagazineCMS.Controllers
 {
@@ -8,17 +10,64 @@ namespace MagazineCMS.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            List<Magazine> openMagazines;
+            List<Magazine> closedMagazines;
+            string facultyName = "";
+
+            if (User.Identity.IsAuthenticated)
+            {
+                string userEmail = User.Identity.Name;
+                int userFaculty = _unitOfWork.User.Get(x => x.Email == userEmail)?.FacultyId ?? 0;
+
+                if (userFaculty != 0)
+                {
+                    facultyName = _unitOfWork.Faculty.Get(x => x.Id == userFaculty)?.Name ?? "";
+                    List<Magazine> magazineList = _unitOfWork.Magazine.GetAll(filter: x => x.FacultyId == userFaculty, includeProperties: "Faculty,Semester").ToList();
+
+                    
+                    closedMagazines = magazineList.Where(m => m.EndDate <= DateTime.Now).ToList();
+                    openMagazines = magazineList.Where(m => m.EndDate > DateTime.Now).ToList();
+                }
+                else
+                {
+                   
+                    return RedirectToAction("Error", "Home");
+                }
+            }
+            else
+            {
+              
+                openMagazines = _unitOfWork.Magazine.GetAll().Where(m => m.EndDate > DateTime.Now).ToList();
+                closedMagazines = _unitOfWork.Magazine.GetAll().Where(m => m.EndDate <= DateTime.Now).ToList();
+            }
+
+            return View(new Tuple<List<Magazine>, List<Magazine>, string>(openMagazines, closedMagazines, facultyName));
         }
 
+        public IActionResult Details(int id)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            var magazine = _unitOfWork.Magazine.Get(x => x.Id == id, includeProperties: "Faculty,Semester");
+            // Fetch conmodel: tribution history for the current user and selected magazine
+            var contributions = _unitOfWork.Contribution.GetAll(
+                filter: c => c.UserId == userId && c.MagazineId == id,
+                includeProperties: "Documents");
+
+            // Pass both magazine and contributions to the view
+            var tuple = new Tuple<Magazine, IEnumerable<Contribution>>(magazine, contributions);
+            return View(tuple);
+        }
         public IActionResult Privacy()
         {
             return View();
