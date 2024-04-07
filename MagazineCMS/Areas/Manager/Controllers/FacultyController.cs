228a0d851a8983e3acc3b4f7180a0db6f6552c86
nguyendoanminhhieu <hieundmgcd210003@fpt.edu.vn>
2024-04-07 22:21:02 +0700

    PP-104---Manager-Create-Faculty

--- MagazineCMS/Areas/Manager/Controllers/FacultyController.cs
+++ MagazineCMS/Areas/Manager/Controllers/FacultyController.cs
@@ -6,6 +6,8 @@ using System;
 using System.Linq;
 using System.Threading.Tasks;
 using MagazineCMS.DataAccess.Repository.IRepository;
+using MagazineCMS.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace MagazineCMS.Areas.Manager.Controllers
 {
@@ -20,153 +22,175 @@ namespace MagazineCMS.Areas.Manager.Controllers
         }
 
         // GET: Manager/Faculty
-        public async Task<IActionResult> Index()
+        /* public async Task<IActionResult> Index()
+         {
+             var faculties = _unitOfWork.Faculty.GetAll().ToList() ;
+             return View(faculties);
+         }*/
+
+        public IActionResult Index(int? id)
         {
-            var faculties = _unitOfWork.Faculty.GetAll().ToList() ;
-            return View(faculties);
+            if (id == null || id == 0)
+            {
+                return View(new Faculty());
+            }
+            Faculty faculty = _unitOfWork.Faculty.Get(s => s.Id == id);
+            return View(faculty);
         }
 
         // GET: Manager/Faculty/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+        /*      public async Task<IActionResult> Details(int? id)
+              {
+                  if (id == null)
+                  {
+                      return NotFound();
+                  }
 
-            var faculty = _unitOfWork.Faculty.Get(m => m.Id == id);
-            if (faculty == null)
-            {
-                return NotFound();
-            }
+                  var faculty = _unitOfWork.Faculty.Get(m => m.Id == id);
+                  if (faculty == null)
+                  {
+                      return NotFound();
+                  }
 
-            return View(faculty);
-        }
-
+                  return View(faculty);
+              }
+      */
         // GET: Manager/Faculty/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-
-        // POST: Manager/Faculty/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Name")] Faculty faculty)
-        {
-            if (ModelState.IsValid)
-            {
-                try
+        /*        public IActionResult Create()
                 {
-                    _unitOfWork.Faculty.Add(faculty);
-                    _unitOfWork.Save();
-                    return RedirectToAction(nameof(Index));
-                }
-                catch (Exception ex)
-                {
-                    ModelState.AddModelError("", "An error occurred while saving the faculty: " + ex.Message);
-                }
-            }
+                    return View();
+                }*/
 
-            return View(faculty);
-        }
 
         // GET: Manager/Faculty/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var faculty =  _unitOfWork.Faculty.Get(f=> f.Id == id);
-            if (faculty == null)
-            {
-                return NotFound();
-            }
-            return View(faculty);
-        }
-
-        // POST: Manager/Faculty/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name")] Faculty faculty)
-        {
-            if (id != faculty.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
+        /*        public async Task<IActionResult> Edit(int? id)
                 {
-                    _unitOfWork.Faculty.Update(faculty);
-                    _unitOfWork.Save();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!FacultyExists(faculty.Id))
+                    if (id == null)
                     {
                         return NotFound();
                     }
-                    else
+
+                    var faculty =  _unitOfWork.Faculty.Get(f=> f.Id == id);
+                    if (faculty == null)
                     {
-                        throw;
+                        return NotFound();
                     }
+                    return View(faculty);
                 }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(faculty);
-        }
+        */
+        // POST: Manager/Faculty/Edit/5
+        /*        [HttpPost]
+                [ValidateAntiForgeryToken]
+                public async Task<IActionResult> Edit(int id, [Bind("Id,Name")] Faculty faculty)
+                {
+                    if (id != faculty.Id)
+                    {
+                        return NotFound();
+                    }
+
+                    if (ModelState.IsValid)
+                    {
+                        try
+                        {
+                            _unitOfWork.Faculty.Update(faculty);
+                            _unitOfWork.Save();
+                        }
+                        catch (DbUpdateConcurrencyException)
+                        {
+                            if (!FacultyExists(faculty.Id))
+                            {
+                                return NotFound();
+                            }
+                            else
+                            {
+                                throw;
+                            }
+                        }
+                        return RedirectToAction(nameof(Index));
+                    }
+                    return View(faculty);
+                }*/
 
         // GET: Delete Faculty
-        [HttpGet]
-        public async Task<IActionResult> Delete(int id)
-        {
-            try
-            {
-                var faculty = _unitOfWork.Faculty.Get(f=> f.Id == id);
-                if (faculty == null)
-                {
-                    return Json(new { success = false, message = "Faculty not found." });
-                }
+        /*     [HttpGet]
+             public async Task<IActionResult> Delete(int id)
+             {
+                 try
+                 {
+                     var faculty = _unitOfWork.Faculty.Get(f=> f.Id == id);
+                     if (faculty == null)
+                     {
+                         return Json(new { success = false, message = "Faculty not found." });
+                     }
 
-                _unitOfWork.Faculty.Remove(faculty);
-                _unitOfWork.Save();
+                     _unitOfWork.Faculty.Remove(faculty);
+                     _unitOfWork.Save();
 
-                return Json(new { success = true, message = "Faculty deleted successfully." });
-            }
-            catch (Exception ex)
-            {
-                return Json(new { success = false, message = "Error deleting faculty: " + ex.Message });
-            }
-        }
+                     return Json(new { success = true, message = "Faculty deleted successfully." });
+                 }
+                 catch (Exception ex)
+                 {
+                     return Json(new { success = false, message = "Error deleting faculty: " + ex.Message });
+                 }
+             }*/
 
 
         // GET: DeleteConfirmed Faculty 
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var faculty = _unitOfWork.Faculty.Get(f=> f.Id == id);
-            if (faculty == null)
-            {
-                return NotFound();
-            }
+        /* public async Task<IActionResult> DeleteConfirmed(int id)
+         {
+             var faculty = _unitOfWork.Faculty.Get(f=> f.Id == id);
+             if (faculty == null)
+             {
+                 return NotFound();
+             }
 
-            _unitOfWork.Faculty.Remove(faculty);
-            _unitOfWork.Save();
-            return RedirectToAction(nameof(Index));
-        }
+             _unitOfWork.Faculty.Remove(faculty);
+             _unitOfWork.Save();
+             return RedirectToAction(nameof(Index));
+         }
 
 
-        private bool FacultyExists(int id)
-        {
-            var faculty = _unitOfWork.Faculty.Get(e => e.Id == id);
-            return (faculty != null) ? true : false;
-        }
+         private bool FacultyExists(int id)
+         {
+             var faculty = _unitOfWork.Faculty.Get(e => e.Id == id);
+             return (faculty != null) ? true : false;
+         }*/
+
 
         #region API CALLS
+
+
+        /*  [HttpGet]
+          public IActionResult GetById(int id)
+          {
+              Faculty faculty = _unitOfWork.Faculty.Get(s => s.Id == id);
+              return Json(new { data = faculty });
+          }*/
+
+        [HttpGet]
+        public IActionResult GetById(int id)
+        {
+            Faculty faculty = _unitOfWork.Faculty.Get(s => s.Id == id);
+            if (faculty == null)
+            {
+                return NotFound(); // Trả về mã trạng thái 404 nếu không tìm thấy Faculty
+            }
+
+            // Lấy số lượng tạp chí và số lượng người dùng của faculty này
+            int magazineCount = _unitOfWork.Magazine.GetAll(m => m.Id == id).ToList().Count;
+            int userCount = _unitOfWork.User.GetAll(u => u.FacultyId == id).ToList().Count;
+
+            // Tạo một đối tượng mới chứa thông tin faculty và số lượng tạp chí, số lượng người dùng
+            var facultyWithCounts = new
+            {
+                Faculty = faculty,
+                MagazineCount = magazineCount,
+                UserCount = userCount
+            };
+
+            return Json(new { data = facultyWithCounts });
+        }
+
 
         [HttpGet]
         public IActionResult GetAll()
@@ -179,6 +203,85 @@ namespace MagazineCMS.Areas.Manager.Controllers
                 UserCount = _unitOfWork.User.GetAll(u => u.FacultyId == faculty.Id).ToList().Count
             }).ToList();
             return Json(new { data = facultiesWithMagazineCount });
+        }
+
+        /*[HttpGet]
+        public IActionResult GetAll()
+        {
+            List<Faculty> facultyList = _unitOfWork.Faculty.GetAll().ToList();
+            return Json(new { data = facultyList });
+        }*/
+
+        /* [HttpPost]
+         public IActionResult Index(Faculty faculty)
+         {
+             if (ModelState.IsValid)
+             {
+                 if (faculty.Id == 0)
+                 {
+                     _unitOfWork.Faculty.Add(faculty);
+                     TempData["success"] = "Faculty created successfully";
+                 }
+                 else
+                 {
+                     _unitOfWork.Faculty.Update(faculty);
+                     TempData["success"] = "Faculty updated successfully";
+                 }
+                 _unitOfWork.Save();
+
+                 return View(faculty);
+             }
+             return View(faculty);
+         }*/
+
+        [HttpPost]
+        public IActionResult Index(Faculty faculty)
+        {
+            if (ModelState.IsValid)
+            {
+                if (faculty.Id == 0)
+                {
+                    _unitOfWork.Faculty.Add(faculty);
+                    TempData["success"] = "Faculty created successfully";
+                }
+                else
+                {
+                    _unitOfWork.Faculty.Update(faculty);
+                    TempData["success"] = "Faculty updated successfully";
+                }
+                _unitOfWork.Save();
+
+                // Sau khi thêm hoặc cập nhật Faculty, chúng ta cần cập nhật lại dữ liệu để hiển thị
+                List<Faculty> faculties = _unitOfWork.Faculty.GetAll().ToList();
+                var facultiesWithMagazineCount = faculties.Select(f => new
+                {
+                    Faculty = faculty,
+                    MagazineCount = _unitOfWork.Magazine.GetAll(m => m.Id == faculty.Id).ToList().Count,
+                    UserCount = _unitOfWork.User.GetAll(u => u.FacultyId == faculty.Id).ToList().Count
+                }).ToList();
+
+                return Json(new { data = facultiesWithMagazineCount });
+            }
+            return View(faculty);
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteById(int id)
+        {
+            Faculty faculty = _unitOfWork.Faculty.Get(s => s.Id == id);
+            Magazine canDelete = _unitOfWork.Magazine.Get(Magazine => Magazine.FacultyId == id);
+            if (faculty == null)
+            {
+                return BadRequest(new { success = false, message = "Error while deleting faculty" });
+            }
+            else if (canDelete != null)
+            {
+                return BadRequest(new { success = false, message = "The Faculty is being used by a magazine" });
+            }
+            _unitOfWork.Faculty.Remove(faculty);
+            _unitOfWork.Save();
+            return Ok(new { success = true, message = "Delete faculty: \"" + faculty.Name + "\" successful" });
+
         }
 
         #endregion
