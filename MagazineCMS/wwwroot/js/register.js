9596b6b3ad5839dfa8f6287e108cc37adce28c9e
TBot <TrongThinh2602@gmail.com>
2024-04-14 15:51:55 +0700

    Fix UI [Guest(Home, Detail), Sidebar, Footer, Some Icon unactive]

---
+++ MagazineCMS/wwwroot/js/register.js
@@ -0,0 +1,11 @@
+﻿/*----------------Terms and Conditions------------------*/
+
+document.getElementById("registerButton").addEventListener("click", function () {
+    var checkbox = document.getElementById("termsCheckbox");
+    var errorMessage = document.getElementById("errorMessage");
+    if (!checkbox.checked) {
+        errorMessage.style.display = "block";
+        return false;
+    }
+    errorMessage.style.display = "none";
+    });
