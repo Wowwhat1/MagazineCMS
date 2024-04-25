9cb87e5fccf29c733fa6ee96eab3db0e3d536f4b
Minh8x8 <triminh701@gmail.com>
2024-04-25 12:52:09 +0700

    No ticket: Account Initializer

--- MagazineCMS.DataAccess/DBInitializer/DBInitializer.cs
+++ MagazineCMS.DataAccess/DBInitializer/DBInitializer.cs
@@ -33,18 +33,85 @@ namespace MagazineCMS.DataAccess.DBInitializer
 
         public void Initialize()
         {
-           /* // Fake firstNames and LastNames
-            List<string> firstNames = new List<string> { "A", "B", "C", "D", "E" };
-            List<string> lastNames = new List<string> { "Nguyen", "Tran", "Le", "Pham", "Hoang" };
-
-            // Create roles if that invalid
             if (!_roleManager.RoleExistsAsync(SD.Role_Student).GetAwaiter().GetResult())
             {
                 _roleManager.CreateAsync(new IdentityRole(SD.Role_Student)).GetAwaiter().GetResult();
                 _roleManager.CreateAsync(new IdentityRole(SD.Role_Coordinator)).GetAwaiter().GetResult();
                 _roleManager.CreateAsync(new IdentityRole(SD.Role_Admin)).GetAwaiter().GetResult();
                 _roleManager.CreateAsync(new IdentityRole(SD.Role_Manager)).GetAwaiter().GetResult();
-            }*/
+
+                _userManager.CreateAsync(new User
+                {
+                    Id = "AdminID1",
+                    UserName = "admin@gmail.com",
+                    Email = "admin@gmail.com",
+                    Firstname = "A",
+                    Lastname = "Nguyen Van",
+                    PhoneNumber = "1112223333",
+                    AvatarUrl = SD.Default_Avatar,
+                    FacultyId = 1,
+                }, "Admin123*").GetAwaiter().GetResult();
+
+                User userDB = _db.Users.FirstOrDefault(u => u.Email == "admin@gmail.com");
+                _userManager.AddToRoleAsync(userDB, SD.Role_Admin).GetAwaiter().GetResult();
+
+                _userManager.CreateAsync(new User
+                {
+                    Id = "ManagerID1",
+                    UserName = "manager@gmail.com",
+                    Email = "manager@gmail.com",
+                    Firstname = "B",
+                    Lastname = "Nguyen Thi",
+                    PhoneNumber = "1112223333",
+                    AvatarUrl = SD.Default_Avatar,
+                    FacultyId = 1,
+                }, "Manager123*").GetAwaiter().GetResult();
+
+                userDB = _db.Users.FirstOrDefault(u => u.Email == "manager@gmail.com");
+                _userManager.AddToRoleAsync(userDB, SD.Role_Manager).GetAwaiter().GetResult();
+
+                _userManager.CreateAsync(new User
+                {
+                    Id = "CoordinatorID1",
+                    UserName = "coordinator@gmail.com",
+                    Email = "coordinator@gmail.com",
+                    Firstname = "C",
+                    Lastname = "Dang Van",
+                    PhoneNumber = "1112223333",
+                    AvatarUrl = SD.Default_Avatar,
+                    FacultyId = 2,
+                }, "Coordinator123*").GetAwaiter().GetResult();
+
+                userDB = _db.Users.FirstOrDefault(u => u.Email == "coordinator@gmail.com");
+                _userManager.AddToRoleAsync(userDB, SD.Role_Coordinator).GetAwaiter().GetResult();
+
+                _userManager.CreateAsync(new User
+                {
+                    Id = "StudentID1",
+                    UserName = "student@gmail.com",
+                    Email = "student@gmail.com",
+                    Firstname = "D",
+                    Lastname = "Dang Thi",
+                    PhoneNumber = "1112223333",
+                    AvatarUrl = SD.Default_Avatar,
+                    FacultyId = 2,
+                }, "Student123**").GetAwaiter().GetResult();
+
+                userDB = _db.Users.FirstOrDefault(u => u.Email == "student@gmail.com");
+                _userManager.AddToRoleAsync(userDB, SD.Role_Student).GetAwaiter().GetResult();
+            }
+            /* // Fake firstNames and LastNames
+             List<string> firstNames = new List<string> { "A", "B", "C", "D", "E" };
+             List<string> lastNames = new List<string> { "Nguyen", "Tran", "Le", "Pham", "Hoang" };
+
+             // Create roles if that invalid
+             if (!_roleManager.RoleExistsAsync(SD.Role_Student).GetAwaiter().GetResult())
+             {
+                 _roleManager.CreateAsync(new IdentityRole(SD.Role_Student)).GetAwaiter().GetResult();
+                 _roleManager.CreateAsync(new IdentityRole(SD.Role_Coordinator)).GetAwaiter().GetResult();
+                 _roleManager.CreateAsync(new IdentityRole(SD.Role_Admin)).GetAwaiter().GetResult();
+                 _roleManager.CreateAsync(new IdentityRole(SD.Role_Manager)).GetAwaiter().GetResult();
+             }*/
 
             //// Loop for create accounts
             //// Create accounts for Computing follow Id from 1 to 100
